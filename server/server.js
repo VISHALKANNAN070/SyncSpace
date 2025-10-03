42ab809a81f41ad40d9fdcf530a5a4e9f9243dad
Vishal Kannan <vishalkannan070@gmail.com>
2025-10-03 19:31:43 +0530

    fix: Update cookie settings for secure transmission and sameSite policy

--- server/server.js
+++ server/server.js
@@ -46,8 +46,8 @@ app.get("/auth/github/callback",
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure:false,
-      sameSite: "lax",
+      secure:true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     })
 
